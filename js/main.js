fd76d72c45324e4c850693c9b21a5023c254f029
Arthom NechayannykH <neahax@gmail.com>
2023-05-13 13:46:57 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// header-slider
+
+const slider = tns({
+    container: '.header-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,    // автоперелист фона
+    controls: false,
+    autoplayButtonOutput: false,
+});
+
+const sliderPrev = document.querySelector('#sliderPrev');
+const sliderNext = document.querySelector('#sliderNext');
+
+sliderPrev.onclick = () => slider.goTo('prev');
+sliderNext.onclick = () => slider.goTo('next');
+
+// Информация по слайдеру
+const currentSliderIndexEl = document.querySelector('#sliderCurrent');
+const sliderTotalEl = document.querySelector('#sliderTotal');
+
+function updateSlideFraction() {
+    const sliderInfo = slider.getInfo();
+    currentSliderIndexEl.innerText = '0' + (sliderInfo.navCurrentIndex + 1);
+    sliderTotalEl.innerText = '0' + sliderInfo.pages;
+}
+
+// На старте
+updateSlideFraction();
+
+// При перемещении
+slider.events.on('indexChanged', () => {
+    updateSlideFraction();
+});
